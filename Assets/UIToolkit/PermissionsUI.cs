fe13e101f4cf322e38147b7eff1c348fe4dd5483
Mikołaj Wirkijowski <lupsik@wp.pl>
2025-04-30 19:18:04 +0200

    Add more scenarios for hk auth

--- Assets/UIToolkit/PermissionsUI.cs
+++ Assets/UIToolkit/PermissionsUI.cs
@@ -6,6 +6,8 @@ public class PermissionsUI : MonoBehaviour
 {
     private VisualElement root;
     private Button allowButton;
+    private Label titleLabel;
+    private Label bodyLabel;
 
     [SerializeField] private GameObject mainGameObject;
     private HealthKitManager healthKitManager; // Reference to the HealthKitManager
@@ -22,6 +24,9 @@ public class PermissionsUI : MonoBehaviour
 
         root = GetComponent<UIDocument>().rootVisualElement;
         allowButton = root.Q<Button>("AllowButton"); // Get the button from the UI document
+        titleLabel = root.Q<Label>("TitleLabel"); // Get the title label from the UI document
+        bodyLabel = root.Q<Label>("BodyLabel"); // Get the body label from the UI document
+        allowButton.SetEnabled(false); // Disable the button initially
         Debug.Log(allowButton); // Log the button for debugging
 
         allowButton.clicked += () => {
@@ -39,24 +44,13 @@ public class PermissionsUI : MonoBehaviour
 
     }
 
-    void AuthorizeHealthKitIOS()
-    {
-        // Get authorization for healthkit
-        healthKitManager.HealthStore.Authorize(healthKitManager.DataTypes, delegate (bool success){
-            if (success)
-            {
-
-            }
-            else
-            {
-                Debug.LogError("HealthKit authorization failed. ");
-            }
-        });
-        
-        var status = healthKitManager.HealthStore.AuthorizationStatusForType(HKDataType.HKQuantityTypeIdentifierDistanceWalkingRunning);
+    void OnAuthorizationProceed() {
+                var status = healthKitManager.HealthStore.AuthorizationStatusForType(HKDataType.HKQuantityTypeIdentifierDistanceWalkingRunning);
 
         if(status == HKAuthorizationStatus.SharingAuthorized)
         {
+            // The user has authorized the app to read the data type
+            // We can now proceed to the game
             Debug.Log("HealthKit authorization status: Authorized");
                 CreateMainGame(); // Call the method to create the main game
                 Destroy(gameObject); // Destroy the permissions UI after authorization
@@ -65,8 +59,38 @@ public class PermissionsUI : MonoBehaviour
         }
         else if (status == HKAuthorizationStatus.SharingDenied)
         {
+            // The user has denied the app permission to read the data type
+            // Show instruction to enable permissions in settings
             Debug.Log("HealthKit authorization status: Denied");
+            titleLabel.text = "Enable Apple Health Access To Continue"; // Change the title label text
+            bodyLabel.text = "To calculate your brick rewards, Fit Bricks needs access to your Walking + Running Distance from Apple Health.\n"
+            + " It looks like access was previously denied.\n" +
+            "1. Go to Settings > Privacy & Security > Health. \n" +
+            "2. Tap Stacking Bricks" +
+            "3. Turn ON \"Walking + Running Distance\"."; // Change the body label text
         }
+        else if (status == HKAuthorizationStatus.NotDetermined){
+            // Not determined means the user has not yet been asked for permission
+            allowButton.SetEnabled(true); // Enable the button to allow the user to authorize
+            allowButton.text = "Allow Access"; // Change the button text to indicate action
+        }
+    }
+    void AuthorizeHealthKitIOS()
+    {
+        // Get authorization for healthkit
+        healthKitManager.HealthStore.Authorize(healthKitManager.DataTypes, delegate (bool success){
+            if (success)
+            {
+                OnAuthorizationProceed();
+            }
+            else
+            {
+                Debug.LogError("HealthKit authorization failed. ");
+            }
+        });
+
+
+        
 
 
     }
